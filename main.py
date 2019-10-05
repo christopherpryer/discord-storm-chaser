33c21eac77c0e306e51b061f66840386a2c6bb9e
Chris Pryer <christophpryer@gmail.com>
2019-10-05 16:28:36 -0400

    tested simple bot messaging

--- main.py
+++ main.py
@@ -3,12 +3,25 @@ import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-token = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+CHANNEL = int(os.getenv('DISCORD_CHANNEL'))
 
 client = discord.Client()
 
+def get_message():
+    return 'Testing bot messages.'
+
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord.')
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print('{client.user} is connected to: {guild.name}(id: {guild.id}).')
 
-client.run(token)
+    # weather-bot channel
+    channel = client.get_channel(CHANNEL)
+
+    msg = get_message()
+    await channel.send(msg)
+    print('Sent: %s using %s.' % (msg, CHANNEL))
+
+client.run(TOKEN)
