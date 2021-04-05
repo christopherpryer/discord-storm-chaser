fe298fa76d453e24016ecc25aa040f9965b79e33
chris <christophpryer@gmail.com>
2021-04-04 20:54:49 -0400

    started last step of weather api use rewrite

--- commands/weather.go
+++ commands/weather.go
@@ -24,7 +24,29 @@ var (
 					Type:        discordgo.ApplicationCommandOptionInteger,
 					Name:        "days-option",
 					Description: "Number of days to pull forecasts for",
-					Required:    false,
+					Choices: []*discordgo.ApplicationCommandOptionChoice{
+						{
+							Name:  "1 day",
+							Value: 1,
+						},
+						{
+							Name:  "3 day",
+							Value: 3,
+						},
+						{
+							Name:  "5 day",
+							Value: 5,
+						},
+						{
+							Name:  "10 day",
+							Value: 10,
+						},
+						{
+							Name:  "14 day",
+							Value: 14,
+						},
+					},
+					Required: false,
 				},
 			},
 		},
@@ -59,16 +81,31 @@ var (
 			},
 		},
 	}
+
 	CommandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
 		"weather": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			margs := []interface{}{
 				i.Data.Options[0].StringValue(),
 				i.Data.Options[1].IntValue(),
 			}
+
 			msgformat := ` > mountain_option: %s `
+			var days int64 = 3
+
 			if len(i.Data.Options) >= 2 {
 				msgformat += "\n> days_option: %d"
+				days = i.Data.Options[1].IntValue()
 			}
+
+			resort := i.Data.Options[0].StringValue()
+			msg, err := getForecast(resort, days)
+
+			msgformat += "\n" + msg
+
+			if err != nil {
+				panic("Forecast failed")
+			}
+
 			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 				// Ignore type for now, we'll discuss them in "responses" part
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
@@ -92,3 +129,7 @@ var (
 		},
 	}
 )
+
+func getForecast(resort string, days int64) (string, error) {
+	return "TODO", nil
+}
